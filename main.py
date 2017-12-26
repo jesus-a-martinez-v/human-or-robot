f107a138fe0bbb032d94351d46ef03efd08028df
Jesús <jesus.a.martinez.v@gmail.com>
2017-12-26 05:09:22 -0500

    Adds main script.

---
+++ main.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from imblearn.over_sampling import SMOTE
+from sklearn import preprocessing
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import f1_score
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import train_test_split
+
+
+def load_data():
+    return pd.read_csv("./data/merged_train.csv")
+
+
+def get_features_and_labels(data):
+    labels = data['outcome']
+    features = data.drop('outcome', axis=1)._get_numeric_data()
+
+    return features, labels
+
+
+def oversample(features, labels):
+    return SMOTE(kind='borderline1').fit_sample(features, labels)
+
+
+def scale(features):
+    return preprocessing.MinMaxScaler().fit_transform(features)
+
+
+data = load_data()
+features, labels = get_features_and_labels(data)
+
+features, labels = oversample(features, labels)
+features = scale(features)
+
+print(features)
+
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+
+print(X_train.shape)
+classifier = RandomForestClassifier()
+
+classifier.fit(X_train, y_train)
+predictions = classifier.predict(X_test)
+
+print("Predictions: ", predictions)
+print("Y_test: ", y_test)
+print("Score: ", roc_auc_score(y_test, predictions, average="samples"))
+print("Accuracy: ", classifier.score(X_test, y_test))
+print("F1 score: ", f1_score(y_test, predictions, pos_label=1.0))
